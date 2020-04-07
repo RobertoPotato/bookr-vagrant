3da09409e9b96545b5d3ec83690a9196f18c527e
RobertoPotato <robertwabuti@gmail.com>
2020-04-07 17:10:37 +0000

    Add /books/{id} Route, modified respective tests and routes

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class BooksController
 {
     public function index()
     {
         return Book::all();
+    }
+
+    public function show($id)
+    {
+        try {
+            return Book::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Book not found'
+                ]
+            ], 404);
+        }
     }
 }
--- tests/app/Http/Controllers/BooksControllerTest.php
+++ tests/app/Http/Controllers/BooksControllerTest.php
@@ -19,8 +19,50 @@ class BooksControllerTest extends TestCase
             ]);
     }
 
+    /** @test **/
     public function index_status_code_should_be_200()
     {
         $this->get('/books')->seeStatusCode(200);
+    }
+
+    /** @test **/
+    public function show_should_return_a_valid_book()
+    {
+        $this
+            ->get('/books/1')
+            ->seeStatusCode(200)
+            ->seeJson([
+                'id' => 1,
+                'title' => 'War of the Worlds',
+                'description' => 'A science fiction masterpiece about Martians invading London',
+                'author' => 'H. G. Wells'
+            ]);
+        $data = json_decode($this->response->getContent(), true);
+        $this->assertArrayHasKey('created_at', $data);
+        $this->assertArrayHasKey('updated_at', $data);
+    }
+
+    /** @test **/
+    public function show_should_fail_when_the_book_id_does_not_exist()
+    {
+        $this
+            ->get('/books/99999')
+            ->seeStatusCode(404)
+            ->seeJson([
+                'error' => [
+                    'message' => 'Book not found'
+                ]
+            ]);
+    }
+
+    /** @test **/
+    public function show_route_should_not_match_an_invalid_route()
+    {
+        $this->get('/books/this-is-invalid');
+        $this->assertNotRegExp(
+            '/Book not found/',
+            $this->response->getContent(),
+            'BooksController@show route matching when it should not.'
+        );
     }
 }
