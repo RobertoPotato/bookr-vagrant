b274aeeb349e258fcab0419dbbcc34e0c7740a15
RobertoPotato <robertwabuti@gmail.com>
2020-04-08 08:03:08 +0000

    create new book

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class BooksController
@@ -23,5 +24,19 @@ class BooksController
                 ]
             ], 404);
         }
+    }
+
+    /**
+     * POST /books
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function store(Request $request)
+    {
+
+        $book = Book::create($request->all());
+        return response()->json(['created' => true], 201, [
+            'Location' => route('books.show', ['id' => $book->id])
+        ]);
     }
 }
